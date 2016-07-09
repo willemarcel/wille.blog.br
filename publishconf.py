e2653ccfe3ddcbfd6b4608c1feb1d261321c20a0
Wille Marcel <wille@wille.blog.br>
2016-07-09 01:08:15 -0300

    fix HEADER_COVER url

--- publishconf.py
+++ publishconf.py
@@ -11,6 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://wille.blog.br'
+HEADER_COVER = SITEURL + '/images/pucon2.jpg'
+
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
